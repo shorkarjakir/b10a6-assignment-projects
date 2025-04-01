e7d2af4515906de37fd4a59178bdffdd8f8199d5
Jakir <shorkarjakir@gmail.com>
2025-04-01 12:46:59 +0600

    fetch and display

--- script/main.js
+++ script/main.js
@@ -9,6 +9,24 @@ const fetchapi = async (url) => {
     }
 };
 
+const categoriesBtn = async (data) => {
+    const pets = await fetchapi(`https://openapi.programming-hero.com/api/peddy/category/${data}`)
+    const petsData = pets.data;
+    displayPets(petsData);
+    activeBtn();
+};
+
+const activeBtn = (data) => {
+   const btn = document.querySelectorAll(`btn-${data}`);
+    console.log(btn);   
+};
+
+const getPets = async (category) => {
+    const pets = await fetchapi('https://openapi.programming-hero.com/api/peddy/pets');
+    const eachPets = pets.pets;
+    displayPets(eachPets);
+};
+
 const getCatagories = async () => {
     const catrgories = await fetchapi('https://openapi.programming-hero.com/api/peddy/categories');
     const eachCategories = catrgories.categories;
@@ -17,20 +35,34 @@ const getCatagories = async () => {
         // console.log(item);
         const div = document.createElement('div');
         div.innerHTML = `
-        <button class="btn md:text-xl lg:text-xl"><img class="w-4 md:w-8 lg:w-8" src="${item.category_icon}" alt="">${item.category}</button>
+        <button id="btn-${item.id}" onclick="categoriesBtn('${item.category}')" class="btn md:text-xl lg:text-xl"><img class="w-4 md:w-8 lg:w-8" src="${item.category_icon}" alt="">${item.category}</button>
         `
         categoriesContainer.append(div);
     })
 };
 
-const getPets = async () => {
-    const pets = await fetchapi('https://openapi.programming-hero.com/api/peddy/pets');
-    const eachPets = pets.pets;
-    console.log(eachPets);
+const displayPets = (pets) => {
     const petsContainer = document.getElementById('pets-container');
-    eachPets.forEach((item) => {
+    petsContainer.innerHTML = '';
+    // console.log(pets);
+    if (pets.length === 0) {
+        petsContainer.classList.remove('grid');
+        petsContainer.innerHTML = `
+        <div class="flex text-center justify-center bg-base-100 shadow-sm">
+          <div class="flex flex-col items-center justify-center">
+            <img class="w-20 md:full lg:w-full" src="./images/error.webp" alt="">
+            <h2 class="text-xl mt-5 md:text-2xl lg:text-3xl">NO Data here</h2>
+            <p class="mt-3 mb-5 text-base">Sorry, we don't have any data here</p>
+          </div>
+        </div>
+        `
+    }
+    else{
+        petsContainer.classList.add('grid');
+    }
+    pets.forEach((item) => {
         const div = document.createElement('div');
-        console.log(item);
+        // console.log(item);
         div.innerHTML = `
       <div class="card bg-base-100 shadow-sm">
          <figure class="">
@@ -48,9 +80,17 @@ const getPets = async () => {
                 <li class="flex gap-1 items-center"> <img class="w-7" src="./images/icons8-gender-48.png" alt=""> Gender: ${item.gender}</li>
                 <li class="flex gap-1 items-center"> <img  class="w-7"src="./images/icons8-price-50.png" alt=""> Price: ${item.price}</li>
            </ul>
-           <div class="card-actions justify-end">
-             <div class="badge badge-outline">Fashion</div>
-             <div class="badge badge-outline">Products</div>
+           <div class="card-actions justify-around mt-4">
+             <button class="btn">
+             <label class="swap swap-flip text-xl">
+                <!-- this hidden checkbox controls the state -->
+                <input type="checkbox" />
+                <div class="swap-off"><img class="w-7" src="./images/icons8-facebook-like-24.png" alt=""></div>
+                <div class="swap-on"><img class="w-7" src="./images/icons8-facebook-like-24 (1).png" alt=""></div>
+            </label>
+            </button>
+             <button class="btn text-green-600 text-bold">Adopt</button>
+             <button class="btn text-green-600 text-bold">Details</button>
            </div>
         </div>
         `
